99ae706541d5fc8c8914439b4907817a16611420
Aaradhy Srivastava <aaradhyarya.srivastava@gmail.com>
2026-07-21 01:03:08 +0530

    Added tags to pressure plate to allow other blueprint actors to activate it

--- Source/DungeonEscape/TriggerComponent.cpp
+++ Source/DungeonEscape/TriggerComponent.cpp
@@ -39,16 +39,22 @@ void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 
 void UTriggerComponent::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (Mover)
+	if (OtherActor->ActorHasTag("PressurePlateActivator"))
 	{
-		Mover->ShouldMove = true;
+		if (Mover)
+		{
+			Mover->ShouldMove = true;
+		}
 	}
 }
 
 void UTriggerComponent::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	if (Mover)
+	if (OtherActor->ActorHasTag("PressurePlateActivator"))
 	{
-		Mover->ShouldMove = false;
+		if (Mover)
+		{
+			Mover->ShouldMove = false;
+		}
 	}
 }
