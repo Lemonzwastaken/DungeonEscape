75f944ef6e4283802fa9a0e5dbe85a4d5f1025de
Aaradhy Srivastava <aaradhyarya.srivastava@gmail.com>
2026-07-21 01:04:31 +0530

    small check to prevent null ptrs

--- Source/DungeonEscape/TriggerComponent.cpp
+++ Source/DungeonEscape/TriggerComponent.cpp
@@ -39,22 +39,28 @@ void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 
 void UTriggerComponent::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor->ActorHasTag("PressurePlateActivator"))
+	if (OtherActor)
 	{
-		if (Mover)
+		if (OtherActor->ActorHasTag("PressurePlateActivator"))
 		{
-			Mover->ShouldMove = true;
+			if (Mover)
+			{
+				Mover->ShouldMove = true;
+			}
 		}
 	}
 }
 
 void UTriggerComponent::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	if (OtherActor->ActorHasTag("PressurePlateActivator"))
+	if (OtherActor)
 	{
-		if (Mover)
+		if (OtherActor->ActorHasTag("PressurePlateActivator"))
 		{
-			Mover->ShouldMove = false;
+			if (Mover)
+			{
+				Mover->ShouldMove = false;
+			}
 		}
 	}
 }
